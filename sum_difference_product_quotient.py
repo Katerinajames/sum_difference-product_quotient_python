40caaeeda389a0a6e39d4898b59204a0d06aec0f
Katerina Paparizou <76427845+Katerinajames@users.noreply.github.com>
2024-09-17 21:17:18 +0300

    Add files via upload

---
+++ sum_difference_product_quotient.py
@@ -0,0 +1,23 @@
+
+k=int(input("Enter an integer\n"))
+
+
+print(" Our integer is ",k)
+
+
+l=int(input("Enter a second  integer\n"))
+
+
+print(" Our second  integer is ",l )
+
+sum=k+l 
+print(" Our sum is %d"%(sum ) )
+
+dif=k-l
+print(" Our difference  is %d"%(dif ) )
+pro=k*l
+print(" Our product   is %d"%(pro) )
+
+quo=k/l
+print(" Our quotient    is %d"%(quo) )
+
